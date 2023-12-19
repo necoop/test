ba006b29a15cd71c8214703e04b58c50cfdd9c7a
Андрей Мокеев <amok2005@gmail.com>
2023-12-19 16:08:58 +0300

    scrolling

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,19 @@
-const video = document.querySelector('video');
-video.controls = false; // Скрыть элементы управления
+const container = document.getElementsByClassName('container')[0];
+const scrollable = document.getElementsByClassName('scrollable');
+
+container.addEventListener('mouseenter', () => {
+    if (!scrollable[scrollable.length - 1].classList.contains('scroll__finished')) {
+        for (let i = 0; i < scrollable.length; i++) {
+            setTimeout(() => {
+                scrollable[i].classList.add('scroll__finished');
+            }, i * 200)
+        }
+    }
+})
+container.addEventListener('mouseleave', () => {
+    if (scrollable[scrollable.length - 1].classList.contains('scroll__finished')) {
+        for (let i = 0; i < scrollable.length; i++) {
+            scrollable[i].classList.remove('scroll__finished');
+        }
+    }
+})
